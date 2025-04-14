6d7df6e55dcf74cca58d443a517f95a5822fd36f
Frederic Bousquet <not@today.com>
2025-04-14 14:52:59 +0100

    can define a number of autobuy which will only be invoices

--- buy_ovh.py
+++ buy_ovh.py
@@ -35,7 +35,7 @@ coupon = configFile['coupon'] if 'coupon' in configFile else ''
 autoBuyList = configFile['auto_buy'] if 'auto_buy' in configFile else []
 autoBuyNum = configFile['auto_buy_num'] if 'auto_buy_num' in configFile else 1
 autoBuyMaxPrice = configFile['auto_buy_max_price'] if 'auto_buy_max_price' in configFile else 0
-autoBuyLastIsInvoice = configFile['auto_buy_last_is_invoice'] if 'auto_buy_last_is_invoice' in configFile else False
+autoBuyInvoicesNum = configFile['auto_buy_num_invoices'] if 'auto_buy_num_invoices' in configFile else 0
 if autoBuyNum == 0:
     autoBuyList = []
 autoBuyNumInit = autoBuyNum
@@ -606,7 +606,9 @@ while True:
                         if autoBuyNum > 0 and plan['availability'] not in unavailableList and plan['autobuy']:
                             # auto buy
                             foundAutoBuyServer = True
-                            autoBuyInvoice = autoBuyLastIsInvoice and autoBuyNum == 1
+                            # The last x are invoices (rather than direct buy) if a number
+                            # of invoices is defined in the config file
+                            autoBuyInvoice = autoBuyNum <= autoBuyInvoicesNum
                             buyServer(plan, not autoBuyInvoice, True)
                             autoBuyNum -= 1
                             if autoBuyNum < 1:
